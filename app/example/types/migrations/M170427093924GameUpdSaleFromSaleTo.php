c08c5169ebedea9e02aabbf7e6a83152115cb44e
perlexed <perlexed@gmail.com>
2017-08-04 17:53:22 +0700

    example types migration fix

--- app/example/types/migrations/M170427093924GameUpdSaleFromSaleTo.php
+++ app/example/types/migrations/M170427093924GameUpdSaleFromSaleTo.php
@@ -7,8 +7,8 @@ use extpoint\yii2\base\Migration;
 class M170427093924GameUpdSaleFromSaleTo extends Migration
 {
     public function up() {
-        $this->alterColumn('example_types_games', 'saleFrom', $this->date());
-        $this->alterColumn('example_types_games', 'saleTo', $this->date());
+        $this->addColumn('example_types_games', 'saleFrom', $this->date());
+        $this->addColumn('example_types_games', 'saleTo', $this->date());
     }
 
     public function down() {
